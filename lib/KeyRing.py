70b08070fde63cafa28901e8bb65ecf6386a772f
Stijn Van Campenhout <stijn@stijn-MS-7181.(none)>
2012-11-17 00:59:29 +0100

    Expanded structure of keyring class (untested)

--- lib/KeyRing.py
+++ lib/KeyRing.py
@@ -18,13 +18,37 @@
 from gi.repository import gnomeKeyring as gk
 class keyring(object):
 	def __init__(self):
+		self.loginDetails = False
 		if gk.is_available() is True:
 			if "GoogleMusic" in gk.list_keyring_names_sync():
 				self.keyring = gk.list_item_ids_sync("GoogleMusic")[1]
 				if "loginDetails" in self.keyring:
-					self.loginDetails = (gk.list_keyring_ids)
+					self.loginDetails = self.get_first_key("GoogleMusic")
 
 			else:
 				gk.create_sync("GoogleMusic","GoogleMusic")
-	def _find_key(key,keyring):
+	def haveLoginDetials():
+		if !self.loginDetails:
+			return False
+		else:
+			return True
+
+	def _find(displayName,keyring):
+		item_keys = gk.list_item_ids_sync(keyring)
+		for k in item_keys[1]:
+			item_info = gk.item_get_info_sync(keyring,k)
+			if item_info.get_display_name() == displayName:
+				return (k,item_info.get_display_name(),item_info.get_secret())
+		return False
+	def _get_first_key(keyring):
+		item_keys = gk.list_item_ids_sync(keyring)
+		item_info = gk.item_get_info_sync(keyring,item_keys[1][0])
+		return (item_info.get_display_name(),item_info.get_secret())
+	def getLoginDetails():
+		return self.loginDetails
+	def saveLoginDetails(user,password):
+		atts = {
+		'useWith':'GoogleMusic'
+		}
+		key = gk.item_create_sync('GoogleMusic',gk.ITEM_GENERIC_SECRET,user,atts,password, True)
 		
