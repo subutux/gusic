c173fc14f4ce0834b8b7bbf10843117b2fe86c2f
Stijn Van Campenhout <stijn.vancampenhout@gmail.com>
2012-11-18 23:36:55 +0100

    added fetchMusicLibrary

--- App.py
+++ App.py
@@ -17,21 +17,22 @@
 
 from gi.repository import Gtk
 from gi.repository import Gdk
+from gi.repository import GLib
 from lib.ui import GoogleMusic_main
 from lib.ui import GoogleMusic_dialog_login
 from lib.KeyRing import keyring
 from gmusicapi.api import Api as gMusicApi
 import threading
 import time
-Gdk.threads_init()
 class Gusic(object):
 	def __init__(self):
 		self.mainBuilder = GoogleMusic_main.Build(self,None)
 		self.main = self.mainBuilder.get_object('window1')
 		self.keyring = keyring()
 		self.api = gMusicApi()
+		self.Library = {}
 		if self.keyring.haveLoginDetails():
-			self.startMain()
+			self.startGusic()
 		else:
 			self.loginBuilder = GoogleMusic_dialog_login.Build(self,None)
 			self.loginDialog = self.loginBuilder.get_object('window_login')
@@ -39,16 +40,22 @@ class Gusic(object):
 			self.image_logo.set_from_file('imgs/Gusic_logo.svg')
 			self.loginDialog.show_all()
 
-	def startMain(self):
+	def startGusic(self,fromLogin=False):
 		self.main.show_all()
+		if fromLogin:
+			self.loginDialog.hide()
 	def isLoggedIn(self):
 		while self.loggedIn == False:
-			time.sleep(2)
+			time.sleep(0.5)
 		return True
+	def fetchMusicLibrary(self):
+		self.Library['songs'] = self.api.get_all_songs()
+
 class main():
 	def __init__(self):
-		app = Gusic()
+		GLib.threads_init()
 		Gdk.threads_enter()
+		app = Gusic()
 		Gtk.main()
 		Gdk.threads_leave()
 
