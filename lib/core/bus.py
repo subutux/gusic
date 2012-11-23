662a9cbb29e90c4e471fabf915084aedc3375577
Stijn Van Campenhout <stijn@cmelectronics.be>
2012-11-23 15:46:43 +0100

    simple event listener/emitter/storer

---
+++ lib/core/bus.py
@@ -0,0 +1,27 @@
+# This file is part of google-music.
+
+# google-music is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# google-music is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with google-music.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Copyright 2012-2013, Stijn Van Campenhout <stijn.vancampenhout@gmail.com>
+class Bus(Object):
+	def __init__(self):
+		self.events = {}
+	def registerEvent(self,event):
+		self.events[event] = [];
+	def listenOn(self,event,function):
+		self.events[event].append(function)
+	def emit(self,event):
+		for function in self.events[event]:
+			function()
+	
