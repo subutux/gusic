b0848ffcc9315cabdc47006c3bf20f12dd86a7f2
Stijn Van Campenhout <stijn@cmelectronics.be>
2012-11-29 19:36:52 +0100

    beginning of sqliteListStore

---
+++ lib/core/sqliteListstore.py
@@ -0,0 +1,17 @@
+# This file is part of google-music.
+
+# google-music is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# google-music is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with google-music.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Copyright 2012-2013, Stijn Van Campenhout <stijn.vancampenhout@gmail.com
+from gi.repository import Gtk
