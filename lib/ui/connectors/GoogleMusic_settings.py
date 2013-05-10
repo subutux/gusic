e69522d192c38a1b8b9d74a97e57bb1eb20f97ee
subutux <stijn.vancampenhout@gmail.com>
2013-05-10 09:55:00 +0200

    begin Settings dialog programming

---
+++ lib/ui/connectors/GoogleMusic_settings.py
@@ -0,0 +1,26 @@
+# This file is part of gusic.
+
+# gusic is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# gusic is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with gusic.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Copyright 2012-2013, Stijn Van Campenhout <stijn.vancampenhout@gmail.com>
+from gi.repository import Gtk
+from lib.core.signals import Signals
+from lib.core import tools
+import threading
+import logging
+import urllib2
+
+class Signals(Signals):
+	def on_click(self,widget):
+		return True
